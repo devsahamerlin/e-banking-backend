1ac8e1fa76ba4a20a0064674c93ec7406e0dd1c8
devsahamerlin <37698308+devsahamerlin@users.noreply.github.com>
2025-07-04 20:34:39 +0400

    feat(security): add oauth2 Authentication and Authorization with InMemory Users

--- src/main/java/com/merlin/digitalbanking/ebankingbackend/web/BankAccountRestController.java
+++ src/main/java/com/merlin/digitalbanking/ebankingbackend/web/BankAccountRestController.java
@@ -6,6 +6,7 @@ import com.merlin.digitalbanking.ebankingbackend.exceptions.BankAccountNotFoundE
 import com.merlin.digitalbanking.ebankingbackend.exceptions.CustomerNotFoundException;
 import com.merlin.digitalbanking.ebankingbackend.services.BankAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +17,25 @@ public class BankAccountRestController {
     private final BankAccountService bankAccountService;
 
     @GetMapping("/accounts/{accountNumber}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public BankAccountDTO getBankAccount(@PathVariable String accountNumber) throws BankAccountNotFoundException {
         return bankAccountService.getBankAccount(accountNumber);
     }
 
     @GetMapping("/customers/{customerId}/accounts")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public List<BankAccountDTO> getCustomerAccounts(@PathVariable Long customerId) throws CustomerNotFoundException {
         return bankAccountService.listCustomerBankAccounts(customerId);
     }
 
     @GetMapping("/accounts/{accountNumber}/operations")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public List<AccountOperationDTO> getAccountHistory(@PathVariable String accountNumber) {
         return bankAccountService.accountHistory(accountNumber);
     }
 
     @GetMapping("/accounts/{accountNumber}/pageOperations")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public AccountHistoryDTO getPageableAccountHistory(
             @PathVariable String accountNumber,
             @RequestParam(name = "page", defaultValue = "0") int page,
@@ -39,16 +44,19 @@ public class BankAccountRestController {
     }
 
     @PostMapping("/accounts/credit")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public void credit(@RequestBody OperationDTO operationDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         bankAccountService.credit(operationDTO.accountId(), operationDTO.amount(), operationDTO.description());
     }
 
     @PostMapping("/accounts/debit")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public void debit(@RequestBody OperationDTO operationDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         bankAccountService.debit(operationDTO.accountId(), operationDTO.amount(), operationDTO.description());
     }
 
     @PostMapping("/accounts/transfer")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public void transfert(@RequestBody TransferDTO transferDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
         bankAccountService.transfer(transferDTO.fromAccountId(), transferDTO.toAccountId(), transferDTO.amount(), transferDTO.description());
     }
--- src/main/java/com/merlin/digitalbanking/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/com/merlin/digitalbanking/ebankingbackend/web/CustomerRestController.java
@@ -5,6 +5,7 @@ import com.merlin.digitalbanking.ebankingbackend.exceptions.CustomerNotFoundExce
 import com.merlin.digitalbanking.ebankingbackend.services.BankAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,31 +13,35 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@CrossOrigin("http://localhost:4200")
 public class CustomerRestController {
     private final BankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public List<CustomerDTO> getCustomers() {
         return bankAccountService.listCustomers();
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
         return bankAccountService.searchCustomers("%"+keyword+"%");
     }
 
     @GetMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_USER') or hasAuthority('SCOPE_ROLE_ADMIN')")
     public CustomerDTO getCustomer(@PathVariable Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
     }
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public CustomerDTO getCustomer(@RequestBody CustomerDTO customerDTO) {
         return bankAccountService.saveCustomer(customerDTO);
     }
 
     @PutMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public CustomerDTO updateCustomers(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO) {
         return bankAccountService
                 .updateCustomer(new CustomerDTO(customerId,
@@ -44,6 +49,7 @@ public class CustomerRestController {
     }
 
     @DeleteMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ROLE_ADMIN')")
     public void deleteCustomer(@PathVariable Long customerId) {
         bankAccountService.deleteCustomer(customerId);
     }
